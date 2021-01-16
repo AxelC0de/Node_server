b1f325d1b0ac79a9b796c3dad54443c49bbbdf69
Redknight777999 <65612144+Redknight777999@users.noreply.github.com>
2021-01-16 22:05:17 +0300

    add: addTodo

--- index.js
+++ index.js
@@ -1,3 +1,33 @@
 const fs = require('fs');
 const http = require('http');
 
+ let addTodo = function(todo, path){
+// 1. Получаем информацию из файла
+let fileContent = fs.readFileSync(path, "utf8");
+let fileContentParsed = JSON.parse(fileContent)
+// console.log(fileContentParsed)
+// 2. находим ключа todoList, закидываем в массив значение todo
+fileContentParsed['todoList'].push(todo)
+// console.log(fileContentParsed)
+// 3. Записать файл с новыми данными
+let fileContentstringified = JSON.stringify(fileContentParsed)
+fs.writeFileSync(path, fileContentstringified)
+
+
+
+}
+
+console.log('Начинаю записывать новой Todo');
+
+addTodo(
+	{
+		todo:'помыть полы',
+		id: new Date().getMilliseconds(),
+		isDone: false
+	},
+	'./state/todo_list.json'
+	);
+
+
+	removeTodo('12345123','./state/todo_list.json')
+console.log('Запись данных завершена');
